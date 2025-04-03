6922fbdd91ae2b3a887ac2b49b876d9a9d93c1e9
Wassim Tout <>
2025-04-03 07:28:02 +0000

    Replaced the release.yml workflow with two new workflows:   ci.yml workflow to run tests anytime there is a commit to the develop, feature/*, and hotfix/* branches. It also merges the feature and hotfix branches to their corresponding destination branch   rba.yml workflow to run tests anytime there is a commit to the release/* branch. It also merges the release branch to both master and develop branches Updated the Bash and yaml code to comply with the coding best practices outlined in shellcheck and ansible-lint Updated the get_secrets_vault function to default localbranch to develop when dealing with feature or release branches Updated the get_secrets_vault function to default localbranch to master when dealing with a hotfix branch

--- Bash/common_parameters.sh
+++ Bash/common_parameters.sh
@@ -1,4 +1,4 @@
-source $(dirname "${0}")/functions_library.sh
+source "$(dirname "${0}")/functions_library.sh"
 
 # Parameters definition
 ANSIBLE_CFG="ansible.cfg"
@@ -9,6 +9,6 @@ ANSIBLE_VERSION='11.1.0'
 ANSIBLE_VARS="vars/datacenters.yml"
 PASSVAULT="vars/passwords.yml"
 REPOVAULT="vars/.repovault.yml"
-CONTAINERWD="/home/ansible/$(basename ${PWD})"
+CONTAINERWD="/home/ansible/$(basename "${PWD}")"
 CONTAINERREPO="registry-1.docker.io/wtout/ansible"
 SECON=$([[ "$(git config --file .git/config user.name|cut -d ' ' -f2 | tr '[:upper:]' '[:lower:]')" == "tout" ]] && echo "false" || echo "true")
