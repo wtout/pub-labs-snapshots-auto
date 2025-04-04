be4be7fb6c7da7fc201872bcd541e585fecddbfb
Wassim Tout <>
2025-04-04 15:59:31 +0000

    Updated the ci and rba workflows to be consistent across all automation code Release Complete

--- Bash/get_common_vault_pass.sh
+++ Bash/get_common_vault_pass.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 password=$(git config --file .git/config remote.origin.url | awk -F '/' '{print $NF}' | sed -e "s|^pub-||")
-[[ "${password}" != *".git" ]] && password="${password}.git"
+[[ ${password} != *".git" ]] && password="${password}.git"
 echo ${password}
